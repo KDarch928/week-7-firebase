045de804e86b68c985eb3f98829c189d71d1fdc0
KDarch928 <Kayle.Drucker@gmail.com>
2018-01-25 01:18:39 -0600

    adding calucations for train time

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -18,7 +18,7 @@ $("#add-train-btn").on("click",function (event) {
     //grab users input
     var trainNam = $("#train-name-input").val().trim();
     var dest = $("#dest-input").val().trim();
-    var startTime = moment($("#time-input").val().trim(), "HH:mm").format("HH:mm");
+    var startTime = moment($("#time-input").val().trim(), "HH:mm").format("h:mm a");
     var freq = $("#freq-input").val().trim();
 
     console.log(trainNam);
@@ -46,3 +46,63 @@ $("#add-train-btn").on("click",function (event) {
     $("#freq-input").val("");
 
 });
+
+//getting the data already stored in firebase
+database.ref().on("child_added", function (childSnapshot, prevChildKey) {
+
+    //create variables and assign the values from the database
+    var trainNam = childSnapshot.val().name;
+    var dest = childSnapshot.val().dest;
+    var starTime = childSnapshot.val().start;
+    var freq = childSnapshot.val().freq;
+
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var startTimeConverted = moment(starTime, "hh:mm").subtract(1, "years");
+    console.log(startTimeConverted);
+
+    // Current Time
+    var currTime = moment();
+    console.log("CURRENT TIME: " + moment(currTime).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(startTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var timeRemainder = diffTime % freq;
+    console.log(timeRemainder);
+
+    // Minute Until Train
+    var minTillTrain = freq - timeRemainder;
+    console.log("MINUTES TILL TRAIN: " + minTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(minTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm a"));
+
+    // Add each train's data into the table
+    $("#train-table > tbody").append("<tr><td>" + trainNam + "</td><td>" + dest + "</td><td>" + freq + "</td><td>" + moment(nextTrain).format("hh:mm a") + "</td><td>" + minTillTrain + "</td><td><button type='button' class='btn btn-link' id='remove' value='"+ trainNam + "'>Remove</button></td></tr>");
+
+    $(document).ready(function () {
+
+        $(".btn").on("click", function (event) {
+            var data = $(this).val().trim();
+            alert(data);
+            var test = childSnapshot;
+            console.log(test);
+
+        });
+
+    });
+});
+
+// $(document).ready(function () {
+
+//     $(".btn").on("click",function (event) {
+//         var data = $(this).val().trim();
+//         alert(data);
+//     });
+
+// });
+
